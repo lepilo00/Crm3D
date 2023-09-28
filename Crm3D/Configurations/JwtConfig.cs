40af18dbeafee4e09b48e2e7ef482d956890a848
Sašo Dedjanski <s.dedjanski@gmail.com>
2023-09-28 22:55:56 +0200

    # comit - adding jwt token auth

---
+++ Crm3D/Configurations/JwtConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Crm3D.Configurations
+{
+    public class JwtConfig
+    {
+        public string Secret { get; set; }
+    }
+}
--- Crm3D/Program.cs
+++ Crm3D/Program.cs
@@ -1,5 +1,9 @@
+using Crm3D.Configurations;
 using Crm3D.Data;
 using Crm3D.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +18,30 @@ builder.Services.AddTransient<ISendEmailService, SendEmailService>();
 
 builder.Services.AddDbContext<DataContext>();
 
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(jwt => 
+    {
+        var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value!);
+
+        jwt.SaveToken = true;
+        jwt.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false, // for dev
+            ValidateAudience = false, // for dev
+            RequireExpirationTime = false, // nastavi na true -> nato ko dodas refresh token
+            ValidateLifetime = true
+        };
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +53,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
